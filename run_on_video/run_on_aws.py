22089dfaeb524e70a374c00313f5c86ca3ce384d
Réda Maizate <reda.maizate@affluences.com>
2023-06-25 16:55:17 +0200

    fix: update code to make it work

--- run_on_video/run_on_aws.py
+++ run_on_video/run_on_aws.py
@@ -131,14 +131,14 @@ def main():
     sts_client = boto3.client('sts', region_name=AWS_REGION, aws_access_key_id=ACCESS_ID, aws_secret_access_key=ACCESS_KEY)
     assumed_role_object = sts_client.assume_role(**role_info)
 
-    ACCESS_ID = assumed_role_object['Credentials']['AccessKeyId']
-    ACCESS_KEY = assumed_role_object['Credentials']['SecretAccessKey']
+    ACCESS_ID_2 = assumed_role_object['Credentials']['AccessKeyId']
+    ACCESS_KEY_2 = assumed_role_object['Credentials']['SecretAccessKey']
     SESSION_TOKEN = assumed_role_object['Credentials']['SessionToken']
     # Get the service resource
-    session = boto3.session.Session(aws_access_key_id=ACCESS_ID, aws_secret_access_key=ACCESS_KEY,
+    session = boto3.session.Session(aws_access_key_id=ACCESS_ID_2, aws_secret_access_key=ACCESS_KEY_2,
                                     aws_session_token=SESSION_TOKEN)
 
-    sqs = session.client('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_ID, aws_secret_access_key=ACCESS_KEY)
+    sqs = session.client('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_ID_2, aws_secret_access_key=ACCESS_KEY_2)
     # sqs = boto3.resource('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_ID, aws_secret_access_key=ACCESS_KEY)
 
     # Get the queue
