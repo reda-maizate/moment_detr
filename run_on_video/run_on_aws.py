8a4f2d6d7a1e6ecff43ba0e5c57acf688c504e53
Réda Maizate <reda.maizate@affluences.com>
2023-06-25 16:37:48 +0200

    fix: update code to make it work

--- run_on_video/run_on_aws.py
+++ run_on_video/run_on_aws.py
@@ -128,7 +128,7 @@ def main():
     # sqs = boto3.resource('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_ID, aws_secret_access_key=ACCESS_KEY)
 
     # Get the queue
-    queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE_NAME, QueueOwnerAWSAccountId=AWS_QUEUE_OWNER_ID)
+    queue = sqs.get_queue_url(QueueName=SQS_QUEUE_NAME)
 
     # Process messages by printing out body and optional author name
     timeout = time.time() + 60 * 3
